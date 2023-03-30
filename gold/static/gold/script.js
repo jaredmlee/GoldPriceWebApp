bafaa98666bbbcc24337d63728a86eb052e1d988
Jared Lee <a02300065@usu.edu>
2023-03-29 22:45:39 -0600

    finished most of the project, just need to finish documentation

--- gold/static/gold/script.js
+++ gold/static/gold/script.js
@@ -7,3 +7,42 @@ fetch(URL)
         date = json.dataset.data[0][0];
         document.querySelector('#price').textContent = "The price of gold as of " + date +" is $"+ data+ " per Troy Ounce";
     });
+
+function calculate(){
+    let input = document.querySelector('#numInput').value;
+    let unit = document.querySelector('#unitSelection').value;
+    if (Number.isNaN(Number(input))){
+        document.querySelector('#error').textContent = "Please provide a valid number!!!!";
+
+    }
+    else{
+        url = "http://localhost:8000/unitconv/convert?from=" + unit + "&to=t_oz&value="+ input;
+        fetch(url)
+            .then(resp => resp.json())
+            .then(json => {
+                if (json.hasOwnProperty('error')) {
+                    throw json.error;
+                }
+                else{
+                    result = json.value * data;
+                    let div = document.createElement('div');
+                    div.setAttribute("class","stuff-box green");
+                    div.setAttribute("id", "result");
+                    div.setAttribute("type", "button");
+                    div.setAttribute("onclick", "remove()");
+                    document.querySelector('body').appendChild(div);
+                    let now = new Date();
+                    let p = document.createElement('p');
+                    p.textContent = now;
+                    let p1 = document.createElement('p');
+                    p1.textContent = input+ " "+ unit+ " of gold is worth$" + result.toFixed(2);
+                    div.appendChild(p);
+                    div.appendChild(p1);
+                }
+            });
+    }
+}
+function remove(){
+    destroy = document.querySelector('#result');
+    destroy.remove()
+}
